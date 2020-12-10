ee92a38cc4b08c3271ffe20205a29a6ce8b42b7d
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-10 16:08:46 +0100

    Milestone1 (Task1 + Task2)

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,9 +1,48 @@
 $(document).ready(function () {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
 
+  class icons {
+    constructor (name, prefix, type, family){
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family;
+    }
+  }
+
+
+  const Icons = [
+    new icons("cat", "fas", "fa-cat", "animals"),
+    new icons("dog", "fas", "fa-cat", "animals"),
+    new icons("dove", "fas", "fa-dove", "animals"),
+    new icons("spider", "fas", "fa-spider", "animals"),
+    new icons("carrot", "fas", "fa-carrot", "vegetables"),
+    new icons("pepper", "fas", "fa-pepper", "vegetables"),
+    new icons("user-ninja", "fas", "fa-user-ninja", "users"),
+    new icons("user-graduate", "fas", "fa-user-graduate", "users"),
+    new icons("user-nurse", "fas", "fa-user-nurse", "users"),
+  ];
+
+  console.log(Icons);
+
+
   // Selezioniamo il container icons
 
+  const container = $('div.icons');
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+
+
+  Icons.forEach(element => {
+
+    console.log(element.prefix, element.type);
+
+    container.innerHTML += `
+    <div>
+      <i class="${element.prefix}${element.type}"></i>
+    </div>`
+
+  })
+
 
 });
 
